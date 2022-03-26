17620b57d648ef91a2fee87799985556ef7c37ae
esaleh32 <97734983+esaleh32@users.noreply.github.com>
2022-03-26 11:26:23 +0300

    Update rac-app.py

--- rac-app.py
+++ rac-app.py
@@ -1,9 +1,25 @@
 import streamlit as st
 from multiapp import MultiApp
-import Home3
-import rca
-import rca2
-import about
+import Home2
+import ndf2
+import cores
+import Help
+import streamlit as st
+import pandas as pd
+import statsmodels.api as sm
+from scipy import stats
+from statsmodels.iolib.table import SimpleTable, default_txt_fmt
+from pandas import read_csv
+import pandas as pd
+from sklearn.model_selection import RepeatedKFold
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import linear_model, preprocessing
+import scipy
+from sklearn.linear_model import LinearRegression
+import statsmodels.formula.api as smf
+import statistics
+import pickle
 
 app = MultiApp()
 
