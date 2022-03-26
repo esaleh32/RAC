cdde573ed2ff4d60b5710cc094a113995626c172
esaleh32 <97734983+esaleh32@users.noreply.github.com>
2022-03-26 11:16:36 +0300

    Update rca2.py

--- rca2.py
+++ rca2.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
-from sklearn.ensemble import RandomForestClassifier
 import streamlit as st
 import pandas as pd
 import xgboost as xgb
